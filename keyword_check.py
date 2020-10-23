c3acbb0e4ccd475b2fc6dc04c663c8411478a84c
Abhijeet Manhas <43112347+abhijeetmanhas@users.noreply.github.com>
2020-10-23 15:07:42 +0530

    Added description to method

--- keyword_check.py
+++ keyword_check.py
@@ -2,6 +2,13 @@ from collections import defaultdict
 import re
 
 def keyword_check(ans,path_file1,path_file2):
+    """
+    A method to find the lines containing specified keywords.
+
+    ans: a variable to store lines containing specified keywords.
+    path_file1: path of file where keywords are to be searched from.
+    path_file2: path of file containing required keywords.
+    """
     file1=open(path_file1,"r")
     file2=open(path_file2,"r")
     include_words = defaultdict(lambda:0)
